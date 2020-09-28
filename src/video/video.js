7b0129c6d6a10d002e0c3341bd91148d93a472f0
superchoby <thomasqtrnh@gmail.com>
2020-09-27 23:34:03 -0700

    using youtube

--- src/video/video.js
+++ src/video/video.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./style.css";
-import ReactPlayer from "react-player";
-import videomp4 from "../media/birthday.mp4";
-import videowebm from "../media/birthday.webm";
+// import ReactPlayer from "react-player";
+// import videomp4 from "../media/birthday.mp4";
+// import videowebm from "../media/birthday.webm";
 import { useHistory } from "react-router-dom";
 
 export function Video() {
@@ -18,11 +18,15 @@ export function Video() {
     <div className="vid-container">
       この動画の質, ごめん&#x1F602;&#x1F602;&#x1F602;
       <div></div>
-      <video controls>
-        <source src={videowebm} type="video/webm" />
-        <source src={videomp4} type="video/mp4" />
-        <p>This browser does not support the video element.</p>
-      </video>
+      <iframe
+        title="birthday"
+        width="800"
+        height="500"
+        src="https://www.youtube.com/embed/f4Kf5JnYqd4"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
       <div onClick={nextPage} className="final-page-button">
         Next Page &#8594;
       </div>
